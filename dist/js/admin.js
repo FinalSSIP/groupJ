ae47e8187beafac68b3b3d417bcbaa8ef1e7a98a
Arga Agung Wiradika <58583726+jarga99@users.noreply.github.com>
2019-12-10 15:14:01 +0700

    Update admin.js
    membuat sidebar agar responsive bila di buka di layar < 768px atau > 768px

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -3,3 +3,16 @@ $(".menu li > a").click(function(e) {
 	// bila a di klik akan nge slide up dan nge slide down 
 	$(".menu ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),e.stopPropagation()
 })
+
+//apabila dibuka di layar yang < 768
+$(window).bind("load resize",function(){
+	if ($(this).width() < 768)
+	{
+		$(".sidebar-collapse").addClass("collapse");
+	}
+	else 
+	{
+		$(".sidebar-collapse").removeClass("collapse");
+		$(".sidebar-collapse").removeAttr("style");
+	}
+})
