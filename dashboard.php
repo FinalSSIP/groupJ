501bc38195d228d433d6c4e174c28137e4848c04
Arga Agung Wiradika <58583726+jarga99@users.noreply.github.com>
2019-12-12 13:34:10 +0700

    Update dashboard.php
    pemecahan halaman dashboard agar dinamis

--- dashboard.php
+++ dashboard.php
@@ -1,61 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	 <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>KIOS MASA DEPAN</title> 
 
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="dist/css/admin.css">
-</head>
-<body>
+<?php 
 
-	<div class="wrapper">
-		<nav class="navbar navbar-default">
-		 <div class="navbar-header">
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".sidebar-collapse" aria-expanded="false">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	     <a class="navbar-brand" href="#">KIOS MASA DEPAN</a>
-    	</nav>
-    </div>
-  		<aside class="sidebar sidebar-collapse">
-			<div class="menu">
-				<ul class="menu-content">
-					<li>
-						<a href="#"><i class="fa fa-home"></i><span> Dashboard</span><i class="fa fa-angle-down pull-right"></i></a>
-						<ul>
-							<li><a href="">profil</a> </li>
-							<li><a href="">user</a> </li>
-							<li><a href="">akun</a></li>
-						</ul>
-					</li>
-					<li>
-						<a href=""><i class="fa fa-cubes" ></i><span> Barang</span></a>
-					</li>
-					<li>
-						<a href=""><i class="fa fa-shopping-cart"></i><span> Penjualan</span></a>
-					</li>
+	require_once 'page/header.php';
+	require_once 'page/sidebar.php';
+	require_once 'page/konten.php';
+	require_once 'page/footer.php';
 
-				</ul>
-			</div>
-		</aside>
-		
-	</div>
-
-
-
-
-
-<!-- jquary.min.js wajib ada ,agar bootrstrap.min.js bisa bekerja -->
-	<script src="dist/js/jquery.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
-	<script src="dist/js/admin.js"></script>
-</body>
-</html>
+ ?>
