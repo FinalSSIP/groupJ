10cb675150acaa4441148973427b68185bc33dd4
Maulana Ramadhan <58567048+maulanaramadhansiregar@users.noreply.github.com>
2019-12-17 16:52:48 +0700

    Delete footer.php

--- page/footer.php
+++
@@ -1,8 +0,0 @@
-
-
-<!-- jquary.min.js wajib ada ,agar bootrstrap.min.js bisa bekerja -->
-	<script src="dist/js/jquery.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
-	<script src="dist/js/admin.js"></script>
-</body>
-</html>
