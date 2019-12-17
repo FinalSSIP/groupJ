9cf84ecd6229ef3e7d34b6e0e9b489f1bf42bd55
Maulana Ramadhan <58567048+maulanaramadhansiregar@users.noreply.github.com>
2019-12-17 17:14:23 +0700

    Update header.php

--- header.php
+++ header.php
@@ -54,3 +54,10 @@ include 'config.php';
 				</ul>
 			</div>
 		</aside>
+	
+	<!-- jquary.min.js wajib ada ,agar bootrstrap.min.js bisa bekerja -->
+	<script src="dist/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="dist/js/admin.js"></script>
+</body>
+</html>
